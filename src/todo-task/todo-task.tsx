8801e6d92720f9a6bbbbeb890fec585ad7c33fb6
UpperCod <mtrujillo@outlook.cl>
2021-08-31 14:35:22 -0400

    fix todo-task

--- src/todo-task/todo-task.tsx
+++ src/todo-task/todo-task.tsx
@@ -1,13 +1,24 @@
-import { Props, c } from "atomico";
+import { Props, c, useProp, DOMEvent } from "atomico";
 import { Checkbox } from "../checkbox/checkbox";
 import tokens from "../tokens.css";
 import style from "./todo-task.css";
 
 function todoTask({ checked }: Props<typeof todoTask.props>) {
+  const [, setChecked] = useProp("checked");
   return (
     <host shadowDom>
       <label>
-        <Checkbox checked={checked} />
+        {/* 
+        In this case we must reflect the checked state 
+        to the component that nests it, in order to 
+        synchronize the state of both the parent and the child 
+        */}
+        <Checkbox
+          checked={checked}
+          onchange={(event: DOMEvent<"change", HTMLInputElement>) =>
+            setChecked(event.target.checked)
+          }
+        />
         <span>
           <slot></slot>
         </span>
